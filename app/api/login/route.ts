28bfc13cd8a53d4824d954a8fc9b5ce71d79f6d0
Ntwali1 <muhizintwali27@gmail.com>
2025-02-13 22:52:39 -0400

    feat: implement login endpoint with cookie management, create idl endpoint to fetch idl classes from /IDL2JS and cache them for faster lookup, edit next config for more vebose logging in dev enviroment

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,6 +1,51 @@
 import { NextResponse } from "next/server";
-
+import { HttpTranslator } from "@/lib/eg-client/src/gateways/http-translator";
 export async function POST(request: Request) {
   const { username, password } = await request.json();
-  return NextResponse.json({ message: "success", status: 200 });
+  const baseUrl = process.env.EG_BASE_URL;
+  if (!baseUrl) {
+    return NextResponse.json({
+      message: "could not find EG_BASE_URL",
+      status: 500,
+    });
+  }
+  const httpTranslator = new HttpTranslator({
+    baseUrl,
+  });
+  const [result] = await httpTranslator.auth.login({
+    username,
+    password,
+    type: "staff",
+  });
+  try {
+    const resultStatus = result.__p.payload.__p.status;
+    if (resultStatus !== "OK") {
+      throw new Error(resultStatus);
+    }
+  } catch (error) {
+    return NextResponse.json({
+      message: "Error: " + error,
+      status: 500,
+    });
+  }
+  const payload = result.__p.payload.__p.content.payload;
+  const response = NextResponse.json({
+    message: "success",
+    status: 200,
+  });
+  response.cookies.set("EG_AUTH_TOKEN", payload.authtoken, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    maxAge: payload.authtime,
+  });
+  response.cookies.set("EG_AUTH_TIME", payload.authtime.toString(), {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    maxAge: payload.authtime,
+  });
+  return response;
 }
