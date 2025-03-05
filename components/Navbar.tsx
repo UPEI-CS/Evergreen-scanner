4d321d7cfb95570e4268098453d56e53a64a2e6d
Muhammad Roufraan Burokur <mroufraan@Muhammads-MBP.ht.home>
2025-03-04 22:49:05 -0400

    implement Home Icon and route

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,19 +7,30 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { LogOut, Settings, User } from "lucide-react";
+import { LogOut, ScanBarcodeIcon, Settings, User } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
+  const router = useRouter(); // Initialize useRouter
+
+  const handleScanIconClick = () => {
+    router.push("/"); // Navigate to the main page
+  };
+
   return (
     <header className="bg-white dark:bg-slate-950 shadow-sm">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <h1 className="text-xl font-bold text-blue-700 dark:text-blue-400">
+        <ScanBarcodeIcon
+          className="h-10 w-10 text-blue-700 cursor-pointer"
+          onClick={handleScanIconClick}
+        />
+        <h1 className="text-2xl font-bold text-blue-700 dark:text-blue-400">
           Evergreen Book Scanner
         </h1>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="ghost" size="icon">
-              <User className="h-6 w-6" />
+              <User className="h-8 w-8" />
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
