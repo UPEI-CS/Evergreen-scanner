2be07b756e1599430922dfad5354f452bef625ce
Muhammad Roufraan Burokur <mroufraan@Muhammads-MBP.ht.home>
2025-03-07 05:28:41 -0400

    Redesign select content's badge

--- components/custom/statusSelect.tsx
+++ components/custom/statusSelect.tsx
@@ -11,7 +11,13 @@ import * as React from "react";
 import { useState } from "react";
 import { toast } from "sonner";
 
-export default function StatusSelect({ status, disabled = false }: { status: string, disabled?: boolean }) {
+export default function StatusSelect({
+  status,
+  disabled = false,
+}: {
+  status: string;
+  disabled?: boolean;
+}) {
   const [selectedStatus, setSelectedStatus] = useState(status);
   const [isLoading, setIsLoading] = React.useState(false);
 
@@ -32,36 +38,60 @@ export default function StatusSelect({ status, disabled = false }: { status: str
     switch (status) {
       case "Available":
         return "success";
-        
     }
-  }
+  };
 
   return (
-    <Select onValueChange={handleStatusChange} disabled={disabled || isLoading} value={selectedStatus}>
-      <SelectTrigger className ="focus:ring-0">
-        <SelectValue >{selectedStatus}</SelectValue>
+    <Select
+      onValueChange={handleStatusChange}
+      disabled={disabled || isLoading}
+      value={selectedStatus}
+    >
+      <SelectTrigger className="focus:ring-0">
+        <SelectValue>{selectedStatus}</SelectValue>
       </SelectTrigger>
       <SelectContent className="rounded-xl bg-white text-blue-600 font-semibold">
         <SelectItem value="Available">
-          <Badge variant="success">Available</Badge>
+          <div className="flex items-center gap-2">
+            <Badge variant="success" className="h-3 w-3"></Badge>
+            <div>Available</div>
+          </div>
         </SelectItem>
         <SelectItem value="Checked Out">
-          <Badge variant="checkedout">Checked Out</Badge>
+          <div className="flex items-center gap-2">
+            <Badge variant="checkedout" className="h-3 w-3"></Badge>
+            <div>Checked Out</div>
+          </div>
         </SelectItem>
         <SelectItem value="Discard/Weed">
-          <Badge variant="discard">Discard/Weed</Badge>
+          <div className="flex items-center gap-2">
+            <Badge variant="discard" className="h-3 w-3"></Badge>
+            <div>Discard/Weed</div>
+          </div>
         </SelectItem>
         <SelectItem value="Missing">
-          <Badge variant="missing">Missing</Badge>
+          <div className="flex items-center gap-2">
+            <Badge variant="missing" className="h-3 w-3"></Badge>
+            <div>Missing</div>
+          </div>
         </SelectItem>
         <SelectItem value="Damaged">
-          <Badge variant="damaged">Damaged</Badge>
+          <div className="flex items-center gap-2">
+            <Badge variant="damaged" className="h-3 w-3"></Badge>
+            <div>Damaged</div>
+          </div>
         </SelectItem>
         <SelectItem value="Lost">
-          <Badge variant="lost">Lost</Badge>
+          <div className="flex items-center gap-2">
+            <Badge variant="lost" className="h-3 w-3"></Badge>
+            <div>Lost</div>
+          </div>
         </SelectItem>
         <SelectItem value="Unknown">
-          <Badge variant="default">Unknown</Badge>
+          <div className="flex items-center gap-2">
+            <Badge variant="default" className="h-3 w-3 "></Badge>
+            <div>Unknown</div>
+          </div>
         </SelectItem>
       </SelectContent>
     </Select>
