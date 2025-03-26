d3484c66f78f49d02cfbeb1606d5f5513aeeeb62
Ntwali1 <muhizintwali27@gmail.com>
2025-03-26 18:46:54 -0300

    refactor: separate login form from page and remove logs in api route

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -2,20 +2,11 @@ import { NextResponse } from "next/server";
 import { client } from "@/lib/eg-client";
 export async function POST(request: Request) {
   const { username, password } = await request.json();
-  const baseUrl = process.env.EG_BASE_URL;
-  if (!baseUrl) {
-    return NextResponse.json(
-      { error: "could not find EG_BASE_URL" },
-      { status: 500 }
-    );
-  }
   const { data, error } = await client.auth.login({
     username,
     password,
     type: "staff",
   });
-  console.log(JSON.stringify(data, null, 2))
-  console.log(JSON.stringify(error, null, 2))
 
   if (error || !data) {
     return NextResponse.json({ error: error }, { status: 401 });
